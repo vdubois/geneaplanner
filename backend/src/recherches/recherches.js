44965b961d7bdd8833e89eccbf455a3a0a8e0da4
Vincent Dubois <v.dubois@lectra.com>
2021-08-13 18:17:58 +0200

    ajout des fonctionnalités de suivi des recherches

--- backend/src/recherches/recherches.js
+++ backend/src/recherches/recherches.js
@@ -17,7 +17,55 @@ module.exports.recupererLesRecherches = async event => {
   if (recherchesDeLUtilisateur) {
     return ok(recherchesDeLUtilisateur);
   }
-  return ok([]);
+  return ok({});
+}
+
+module.exports.ajouterDesRecherchesDIndividu = async event => {
+  const utilisateur = utilisateurConnecte(event);
+  if (event.pathParameters.identifiant !== utilisateur.email) {
+    return unauthorized(`Non autorisé pour le compte ${utilisateur.email}`);
+  }
+  if (event.body) {
+    const individu = JSON.parse(event.body);
+    const partitionKey = `${utilisateur.email}#recherches`;
+    let recherchesDeLUtilisateur = await dynamoDBRepository.findOneByPartitionKey(partitionKey);
+    if (!recherchesDeLUtilisateur) {
+      recherchesDeLUtilisateur = {
+        partitionKey,
+        recherches: {
+        }
+      };
+      if (!recherchesDeLUtilisateur.recherches[individu]) {
+        recherchesDeLUtilisateur.recherches[individu] = {
+          individu: individu.identifiant,
+          priorite: 'moyenne'
+        };
+      }
+    } else {
+      recherchesDeLUtilisateur.recherches[individu] = {
+        individu: individu.identifiant,
+        priorite: 'moyenne'
+      };
+    }
+    await dynamoDBRepository.save(recherchesDeLUtilisateur);
+    return created(individu);
+  }
+  return badRequest('Les informations de l\'individu sont obligatoires');
+}
+
+module.exports.supprimerDesRecherchesDIndividu = async event => {
+  const utilisateur = utilisateurConnecte(event);
+  if (event.pathParameters.identifiant !== utilisateur.email) {
+    return unauthorized(`Non autorisé pour le compte ${utilisateur.email}`);
+  }
+  const individu = event.pathParameters.individu;
+  const partitionKey = `${utilisateur.email}#recherches`;
+  let recherchesDeLUtilisateur = await dynamoDBRepository.findOneByPartitionKey(partitionKey);
+  if (recherchesDeLUtilisateur && recherchesDeLUtilisateur.recherches) {
+    delete recherchesDeLUtilisateur.recherches[individu];
+    await dynamoDBRepository.save(recherchesDeLUtilisateur);
+  }
+  return noContent();
 }
 
 module.exports.ajouterUneNoteAUnIndividu = async event => {
@@ -29,22 +77,22 @@ module.exports.ajouterUneNoteAUnIndividu = async event => {
     const note = JSON.parse(event.body);
     note.id = uuid.v4();
     const partitionKey = `${utilisateur.email}#recherches`;
-    let recherchesDeLIndividu = await dynamoDBRepository.findOneByPartitionKey(partitionKey);
-    if (!recherchesDeLIndividu) {
-      recherchesDeLIndividu = {
+    let recherchesDeLUtilisateur = await dynamoDBRepository.findOneByPartitionKey(partitionKey);
+    if (!recherchesDeLUtilisateur) {
+      recherchesDeLUtilisateur = {
         partitionKey,
         recherches: {
         }
       };
-      if (!recherchesDeLIndividu.recherches[event.pathParameters.individu]) {
-        recherchesDeLIndividu.recherches[event.pathParameters.individu] = {
+      if (!recherchesDeLUtilisateur.recherches[event.pathParameters.individu]) {
+        recherchesDeLUtilisateur.recherches[event.pathParameters.individu] = {
           notes: [note]
         };
       }
     } else {
-      recherchesDeLIndividu.recherches[event.pathParameters.individu].notes.push(note);
+      recherchesDeLUtilisateur.recherches[event.pathParameters.individu].notes.push(note);
     }
-    await dynamoDBRepository.save(recherchesDeLIndividu);
+    await dynamoDBRepository.save(recherchesDeLUtilisateur);
     return created(note);
   }
   return badRequest('Les informations de la note sont obligatoires');
@@ -90,10 +138,9 @@ module.exports.supprimerUneNoteDUnIndividu = async event => {
     || !recherchesDeLUtilisateur.recherches[event.pathParameters.individu]
     || !recherchesDeLUtilisateur.recherches[event.pathParameters.individu].notes.find(note => note.id === event.pathParameters.note)) {
     return badRequest(`La note n'existe pas`);
-  } else {
-    recherchesDeLUtilisateur.recherches[event.pathParameters.individu].notes =
-      recherchesDeLUtilisateur.recherches[event.pathParameters.individu].notes.filter(note => note.id !== event.pathParameters.note);
   }
+  recherchesDeLUtilisateur.recherches[event.pathParameters.individu].notes =
+    recherchesDeLUtilisateur.recherches[event.pathParameters.individu].notes.filter(note => note.id !== event.pathParameters.note);
   await dynamoDBRepository.save(recherchesDeLUtilisateur);
   return noContent();
 }
